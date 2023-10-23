9b60bb2da3e0126ebb18073d19ca7afebca83975
Eric Hong <f102041332@gmail.com>
2023-10-23 23:14:03 +0800

    [feature] i18中文 及部分英文

--- i18n.js
+++ i18n.js
@@ -1,32 +1,32 @@
-let currentLanguage = "en";
+let currentLanguage = 'zh';
 
 function loadLanguageFile(language) {
-	fetch(`./i18n/${language}.json`)
-		.then((response) => response.json())
-		.then((data) => {
-			// 更新页面上的文本
-			const elements = document.querySelectorAll("[data-i18n]");
-			elements.forEach((element) => {
-				const key = element.getAttribute("data-i18n");
-				if (data[key]) {
-					element.textContent = data[key];
-				}
-			});
-		})
-		.catch((error) => console.error("Error loading language file", error));
+  fetch(`./i18n/${language}.json`)
+    .then(response => response.json())
+    .then(data => {
+      // 更新页面上的文本
+      const elements = document.querySelectorAll('[data-i18n]');
+      elements.forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        if (data[key]) {
+          element.textContent = data[key];
+        }
+      });
+    })
+    .catch(error => console.error('Error loading language file', error));
 }
 
 // init
 loadLanguageFile(currentLanguage);
 
 function changeLanguage(language) {
-	currentLanguage = language;
-	loadLanguageFile(language);
+  currentLanguage = language;
+  loadLanguageFile(language);
 }
 
-const languageButtons = document.querySelectorAll(".language-button");
-languageButtons.forEach((button) => {
-	button.addEventListener("click", () => {
-		changeLanguage(button.getAttribute("data-language"));
-	});
+const languageButtons = document.querySelectorAll('.language-button');
+languageButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    changeLanguage(button.getAttribute('data-language'));
+  });
 });
